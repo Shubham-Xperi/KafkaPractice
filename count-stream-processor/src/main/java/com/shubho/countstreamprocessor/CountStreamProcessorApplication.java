9cdd631a551f9748cf339297628a0e7c0ff52be5
Shubham Prasad <shubham.prasad@xperi.com>
2023-08-15 16:03:33 +0530

    Adding count stream processor for processing data from random-data-topic

---
+++ count-stream-processor/src/main/java/com/shubho/countstreamprocessor/CountStreamProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.shubho.countstreamprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountStreamProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountStreamProcessorApplication.class, args);
+	}
+
+}
---
+++ count-stream-processor/src/test/java/com/shubho/countstreamprocessor/CountStreamProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubho.countstreamprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountStreamProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
